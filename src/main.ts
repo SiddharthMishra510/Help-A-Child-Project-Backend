fc27ae86723ee8fd8695de4216a334293259af3a
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-19 03:38:55 +0530

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
 
+  // Enable CORS for requests from same localhost machine
+  app.enableCors({
+    origin: 'http://localhost:3005',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(port);
   console.log(`🚀 Server running on http://localhost:${port}`);
 }
