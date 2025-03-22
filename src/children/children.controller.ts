4e3c1142f0a0ddf179525901cfa2f82ebe2e2a77
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-23 03:03:27 +0530

    feat: handle amount donation requests

--- src/children/children.controller.ts
+++ src/children/children.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Post, Body, Delete } from "@nestjs/common";
-import { CreateChildDto } from "./dto/create-children.dto";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Put,
+  Param,
+  NotFoundException,
+} from "@nestjs/common";
+import { CreateChildDto } from "./dto/create-child.dto";
 import { ChildrenService } from "./children.service";
 import { Child } from "./interfaces/child.interface";
 
@@ -15,6 +24,11 @@ export class ChildrenController {
   @Get()
   async findAll(): Promise<Child[]> {
     return this.childrenService.findAll();
+  }
+
+  @Put(":id/donate")
+  async addDonation(@Param("id") id: string, @Body() body: { amount: number }) {
+    return this.childrenService.addDonation(id, body.amount);
   }
 
   @Delete()
--- src/children/children.service.ts
+++ src/children/children.service.ts
@@ -1,9 +1,14 @@
 import { Child } from "./interfaces/child.interface";
 import { db } from "../db/db.module";
 import { children } from "../db/schema";
-import { Inject, ConflictException } from "@nestjs/common";
+import {
+  Inject,
+  ConflictException,
+  NotFoundException,
+  BadRequestException,
+} from "@nestjs/common";
 import { eq } from "drizzle-orm";
-import { CreateChildDto } from "./dto/create-children.dto";
+import { CreateChildDto } from "./dto/create-child.dto";
 
 export class ChildrenService {
   constructor(@Inject("DATABASE") private readonly database: typeof db) {}
@@ -40,5 +45,31 @@ export class ChildrenService {
 
   async deleteAll() {
     await this.database.delete(children);
+  }
+
+  async addDonation(id: string, amount: number): Promise<Child> {
+    if (!number.amount || number.amount <= 0) {
+      throw new BadRequestException("Invalid donation amount.");
+    }
+
+    const existingChild = await db
+      .select()
+      .from(children)
+      .where(eq(children.id, id))
+      .limit(1);
+
+    if (existingChild.length === 0) {
+      throw new NotFoundException(`Child with id ${id} not found.`);
+    }
+
+    const currentAmount = existingChild[0].amountDonated || 0;
+
+    const updatedChild: Child[] = await db
+      .update(children)
+      .set({ amountDonated: currentAmount + amount })
+      .where(eq(children.id, id))
+      .returning();
+
+    return updatedChild[0];
   }
 }
