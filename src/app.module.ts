b0cd0cdd18cf8f66cdabcec93ee4d8d290706b39
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-24 22:05:53 +0530

    feat: fix process.env not being loaded issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { ChildrenModule } from "./children/children.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [ChildrenModule],
+  imports: [ConfigModule.forRoot(), ChildrenModule],
 })
 export class AppModule {}
