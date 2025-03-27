f8035d6d96cef17ed108743a2368fe26d03f56e3
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-28 04:11:37 +0530

    chore: update drizzle config to use env variables

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -2,13 +2,16 @@ import { Module } from "@nestjs/common";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new Pool({
-  user: "nestuser",
-  password: "nestpassword",
-  host: "localhost",
-  port: 5432,
-  database: "nestdb",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
 });
 
 export const db = drizzle(pool, { schema });
