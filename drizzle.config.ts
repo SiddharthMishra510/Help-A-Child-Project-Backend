b36bd746d426dbd6577b7f6462a90a8308239808
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-28 03:41:58 +0530

    chore: file cleanup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
-  dialect: "postgresql", // ✅ Keep "postgresql"
+  dialect: "postgresql",
   dbCredentials: {
     host: process.env.DB_HOST!,
     port: Number(process.env.DB_PORT!) || 5432,
