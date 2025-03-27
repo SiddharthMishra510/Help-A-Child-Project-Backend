4d26548a98328e3baf361a2bc9753e4c32c9eea9
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-28 03:39:56 +0530

    chore: pass path to .env in ConfigModule's forRoot()

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,13 @@ import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), ChildrenModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    ChildrenModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
